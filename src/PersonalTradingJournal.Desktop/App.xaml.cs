d51a5d250635c50dcb60588a1e286178c130b50f
Yovko Garkov <iovko1993@gmail.com>
2026-09-07 19:24:53 +0300

    Bootstrap WPF with Generic Host and DI

--- src/PersonalTradingJournal.Desktop/App.xaml.cs
+++ src/PersonalTradingJournal.Desktop/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
 
 namespace PersonalTradingJournal.Desktop;
@@ -8,5 +9,36 @@ namespace PersonalTradingJournal.Desktop;
 /// </summary>
 public partial class App : Application
 {
-}
+    private readonly IHost _host;
 
+    public App()
+    {
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+        builder.Services.AddTransient<MainWindow>();
+
+        _host = builder.Build();
+    }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        await _host.StartAsync();
+
+        MainWindow mainWindow = _host.Services.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        try
+        {
+            await _host.StopAsync();
+        }
+        finally
+        {
+            _host.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
